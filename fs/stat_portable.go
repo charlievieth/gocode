6b84e0a3bc92c4b0494668f3cfd46a2f9167b74a
Charlie Vieth <charlie.vieth@gmail.com>
2017-04-07 23:18:04 -0400

    fs: move fileStat definition to stat_unix.go

--- fs/stat_portable.go
+++ fs/stat_portable.go
@@ -6,10 +6,16 @@ import (
 	"os"
 )
 
+// Lstat returns a FileInfo describing the named file.
+// If the file is a symbolic link, the returned FileInfo
+// describes the symbolic link. Lstat makes no attempt to follow the link.
+// If there is an error, it will be of type *PathError.
 func Lstat(name string) (os.FileInfo, error) {
 	return os.Lstat(name)
 }
 
+// Stat returns a FileInfo describing the named file.
+// If there is an error, it will be of type *PathError.
 func Stat(name string) (os.FileInfo, error) {
 	return os.Stat(name)
 }
--- fs/stat_unix.go
+++ fs/stat_unix.go
@@ -5,8 +5,39 @@ package fs
 import (
 	"os"
 	"syscall"
+	"time"
 )
 
+// A fileStat is the implementation of FileInfo returned by Stat and Lstat,
+// that implements the GobEncode, GobDecode, MarshalJSON and UnmarshalJSON.
+// Sys() always returns nil.
+type fileStat struct {
+	name    string
+	size    int64
+	mode    os.FileMode
+	modTime time.Time
+}
+
+// Name, base name of the file
+func (fs *fileStat) Name() string { return fs.name }
+
+// Size, length in bytes for regular files; system-dependent for others
+func (fs *fileStat) Size() int64 { return fs.size }
+
+// Mode, file mode bits
+func (fs *fileStat) Mode() os.FileMode { return fs.mode }
+
+// ModTime, modification time
+func (fs *fileStat) ModTime() time.Time { return fs.modTime }
+
+// IsDir, abbreviation for Mode().IsDir()
+func (fs *fileStat) IsDir() bool { return fs.mode.IsDir() }
+
+// Sys, underlying data source, unlike os.FileInfo nil is always returned.
+func (fs *fileStat) Sys() interface{} { return nil }
+
+// Stat returns a FileInfo describing the named file.
+// If there is an error, it will be of type *PathError.
 func Stat(name string) (os.FileInfo, error) {
 	var sys syscall.Stat_t
 	err := syscall.Stat(name, &sys)
@@ -18,6 +49,10 @@ func Stat(name string) (os.FileInfo, error) {
 	return &f, nil
 }
 
+// Lstat returns a FileInfo describing the named file.
+// If the file is a symbolic link, the returned FileInfo
+// describes the symbolic link. Lstat makes no attempt to follow the link.
+// If there is an error, it will be of type *PathError.
 func Lstat(name string) (os.FileInfo, error) {
 	var sys syscall.Stat_t
 	err := syscall.Lstat(name, &sys)
