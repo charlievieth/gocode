664a6e8fe5baf70901ef11b2175835fb324052ea
Charlie Vieth <charlie.vieth@gmail.com>
2018-04-15 16:56:26 -0400

    Fix failure in test 0055 introduced by: d7aee8965
    Commit d7aee8965 broke test 0055 as setting the decl_visited in
    find_child_and_in_embedded prevents other type inference methods from
    running.

--- decl.go
+++ decl.go
@@ -67,6 +67,8 @@ const (
 
 	// for preventing infinite recursions and loops in type inference code
 	decl_visited
+
+	decl_visited_find_child_and_in_embedded
 )
 
 //-------------------------------------------------------------------------
@@ -392,12 +394,24 @@ func (d *decl) is_visited() bool {
 	return d.flags&decl_visited != 0
 }
 
+func (d *decl) is_visited_find_child_and_in_embedded() bool {
+	return d.flags&decl_visited_find_child_and_in_embedded != 0
+}
+
 func (d *decl) set_visited() {
 	d.flags |= decl_visited
 }
 
+func (d *decl) set_visited_find_child_and_in_embedded() {
+	d.flags |= decl_visited_find_child_and_in_embedded
+}
+
 func (d *decl) clear_visited() {
 	d.flags &^= decl_visited
+}
+
+func (d *decl) clear_visited_find_child_and_in_embedded() {
+	d.flags &^= decl_visited_find_child_and_in_embedded
 }
 
 func (d *decl) expand_or_replace(other *decl) {
@@ -1029,6 +1043,12 @@ func (d *decl) find_child_and_in_embedded(name string) *decl {
 	if d == nil {
 		return nil
 	}
+
+	if d.is_visited_find_child_and_in_embedded() {
+		return nil
+	}
+	d.set_visited_find_child_and_in_embedded()
+	defer d.clear_visited_find_child_and_in_embedded()
 
 	c := d.find_child(name)
 	if c == nil {
