6b293736496cfa53bb5f1aaffa61f6a654093d12
Charlie Vieth <cvieth@lyft.com>
2018-08-29 12:31:53 -0400

    WIP: benchmark suggest

--- suggest/suggest_test.go
+++ suggest/suggest_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 
 	"github.com/charlievieth/gocode/suggest"
@@ -75,4 +76,115 @@ func testRegress(t *testing.T, testDir string) {
 		t.Errorf("%s:\nGot:\n%s\nWant:\n%s\n", testDir, got, want)
 		return
 	}
+}
+
+type suggestBenchmark struct {
+	filename string
+	data     []byte
+	cursor   int
+}
+
+var loadSuggestBenchmarksOnce sync.Once
+var suggestBenchmarks []*suggestBenchmark
+
+func newBenchmark(b *testing.B, testDir string) *suggestBenchmark {
+	testDir, err := filepath.Abs(testDir)
+	if err != nil {
+		b.Errorf("Abs failed: %v", err)
+		return nil
+	}
+
+	filename := filepath.Join(testDir, "test.go.in")
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		b.Errorf("ReadFile failed: %v", err)
+		return nil
+	}
+
+	cursor := bytes.IndexByte(data, '@')
+	if cursor < 0 {
+		b.Errorf("Missing @")
+		return nil
+	}
+	data = append(data[:cursor], data[cursor+1:]...)
+
+	return &suggestBenchmark{
+		filename: filename,
+		data:     data,
+		cursor:   cursor,
+	}
+}
+
+func loadBenchmarks(b *testing.B) {
+	testDirs, err := filepath.Glob("testdata/test.*")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for _, testDir := range testDirs {
+		suggestBenchmarks = append(suggestBenchmarks, newBenchmark(b, testDir))
+	}
+}
+
+func BenchmarkOne(b *testing.B) {
+	loadSuggestBenchmarksOnce.Do(func() {
+		loadBenchmarks(b)
+	})
+	cfg := suggest.Config{
+		Importer: importer.Default(),
+	}
+	test := suggestBenchmarks[0]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		cfg.Suggest(test.filename, test.data, test.cursor)
+	}
+}
+
+func BenchmarkTen(b *testing.B) {
+	loadSuggestBenchmarksOnce.Do(func() {
+		loadBenchmarks(b)
+	})
+	cfg := suggest.Config{
+		Importer: importer.Default(),
+	}
+	tests := suggestBenchmarks[0:10]
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, x := range tests {
+			cfg.Suggest(x.filename, x.data, x.cursor)
+		}
+	}
+}
+
+func BenchmarkAll(b *testing.B) {
+	loadSuggestBenchmarksOnce.Do(func() {
+		loadBenchmarks(b)
+	})
+	cfg := suggest.Config{
+		Importer: importer.Default(),
+	}
+	tests := suggestBenchmarks
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for _, x := range tests {
+			cfg.Suggest(x.filename, x.data, x.cursor)
+		}
+	}
+}
+
+func BenchmarkParallel(b *testing.B) {
+	loadSuggestBenchmarksOnce.Do(func() {
+		loadBenchmarks(b)
+	})
+	tests := suggestBenchmarks
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		cfg := suggest.Config{
+			Importer: importer.Default(),
+		}
+		for pb.Next() {
+			for _, x := range tests {
+				cfg.Suggest(x.filename, x.data, x.cursor)
+			}
+		}
+	})
 }
