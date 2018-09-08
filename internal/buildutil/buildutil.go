de14b38e012b85cb697cc453871162ba0c117fed
Charlie Vieth <cvieth@lyft.com>
2018-09-08 15:04:11 -0400

    buildutil: remove invalid comment
    The comment was about adding support for binary only packages, but we
    will only be dealing with source packages here.

--- internal/buildutil/buildutil.go
+++ internal/buildutil/buildutil.go
@@ -44,8 +44,6 @@ func ShortImport(ctxt *build.Context, path string) (string, bool) {
 
 var slashslash = []byte("//")
 
-// TODO (CEV): Add support for binary only packages
-//
 // shouldBuild reports whether it is okay to use this file,
 // The rule is that in the file's leading run of // comments
 // and blank lines, which must be followed by a blank line
