7e4cd1b94b349580efb3aa32ffdf1d9082064899
Charlie Vieth <cvieth@lyft.com>
2018-09-08 14:42:59 -0400

    buildutil: remove readImports and test readImportsFast

--- internal/buildutil/read_test.go
+++ internal/buildutil/read_test.go
@@ -5,7 +5,6 @@
 package buildutil
 
 import (
-	"bytes"
 	"go/build"
 	"io"
 	"strings"
@@ -14,217 +13,138 @@ import (
 
 const quote = "`"
 
-type readTest struct {
+type readFastTest struct {
 	// Test input contains ℙ where readImports should stop.
 	in  string
+	out string
 	err string
 }
 
-var readImportsTests = []readTest{
+var readFastTests = []readFastTest{
 	{
-		`package p`,
+		"package p",
+		"package p",
+		"",
+	},
+	{
+		"package p;",
+		"package p;",
+		"",
+	},
+	{
+		"// +build linux\npackage p;",
+		"// +build linux\npackage p;",
+		"",
+	},
+	{
+		"// foo /\npackage p;",
+		"// foo /\npackage p;",
+		"",
+	},
+	{
+		"/* foo */\npackage p;",
+		"/* foo */\npackage p;",
+		"",
+	},
+	{
+		"/* foo */package p",
+		"/* foo */package p",
+		"",
+	},
+	{
+		`/*
+		  * a
+		  * long
+		  * comment
+		 */
+		 // foo
+	     package p
+		`,
+		`/*
+		  * a
+		  * long
+		  * comment
+		 */
+		 // foo
+	     ` + "package p\n",
+		"",
+	},
+	{
+		"// Copyright 2012 The Go Authors.  All rights reserved.\n" +
+			"//+build linux\n" +
+			"package p\n" +
+			"import .\n",
+		"// Copyright 2012 The Go Authors.  All rights reserved.\n" +
+			"//+build linux\n" +
+			"package p\n",
 		"",
 	},
 	{
 		`package p; import "x"`,
+		"package p;",
 		"",
 	},
 	{
 		`package p; import . "x"`,
+		"package p;",
+		"",
+	},
+	// syntax error in import clause, but not package
+	{
+		"package p; import",
+		"package p;",
 		"",
 	},
 	{
-		`package p; import "x";ℙvar x = 1`,
+		"packge misspelled",
 		"",
+		"syntax error",
 	},
 	{
-		`package p
-
-		// comment
-
-		import "x"
-		import _ "x"
-		import a "x"
-
-		/* comment */
-
-		import (
-			"x" /* comment */
-			_ "x"
-			a "x" // comment
-			` + quote + `x` + quote + `
-			_ /*comment*/ ` + quote + `x` + quote + `
-			a ` + quote + `x` + quote + `
-		)
-		import (
-		)
-		import ()
-		import()import()import()
-		import();import();import()
-
-		ℙvar x = 1
-		`,
 		"",
+		"",
+		"syntax error",
+	},
+	{
+		"package",
+		"",
+		"syntax error",
+	},
+	{
+		"/ bad comment\npackage p;",
+		"",
+		"syntax error",
+	},
+	{
+		"package p\n\x00\nimport `math`\n",
+		"p",
+		"unexpected NUL in input",
 	},
 }
 
-var readCommentsTests = []readTest{
-	{
-		`ℙpackage p`,
-		"",
-	},
-	{
-		`ℙpackage p; import "x"`,
-		"",
-	},
-	{
-		`ℙpackage p; import . "x"`,
-		"",
-	},
-	{
-		`// foo
-
-		/* bar */
-
-		/* quux */ // baz
-
-		/*/ zot */
-
-		// asdf
-		ℙHello, world`,
-		"",
-	},
-}
-
-func testRead(t *testing.T, tests []readTest, read func(io.Reader) ([]byte, error)) {
-	for i, tt := range tests {
-		var in, testOut string
-		j := strings.Index(tt.in, "ℙ")
-		if j < 0 {
-			in = tt.in
-			testOut = tt.in
-		} else {
-			in = tt.in[:j] + tt.in[j+len("ℙ"):]
-			testOut = tt.in[:j]
-		}
-		r := strings.NewReader(in)
-		buf, err := read(r)
+func TestReadImportsFast(t *testing.T) {
+	for i, tt := range readFastTests {
+		buf, err := readImportsFast(strings.NewReader(tt.in))
 		if err != nil {
 			if tt.err == "" {
-				t.Errorf("#%d: err=%q, expected success (%q)", i, err, string(buf))
+				t.Errorf("#%d (%+v): err=%q, expected success (%q)", i, tt, err, string(buf))
 				continue
 			}
 			if !strings.Contains(err.Error(), tt.err) {
-				t.Errorf("#%d: err=%q, expected %q", i, err, tt.err)
+				t.Errorf("#%d (%+v): err=%q, expected %q", i, tt, err, tt.err)
 				continue
 			}
 			continue
 		}
 		if err == nil && tt.err != "" {
-			t.Errorf("#%d: success, expected %q", i, tt.err)
+			t.Errorf("#%d (%+v): success, expected %q", i, tt, tt.err)
 			continue
 		}
-
 		out := string(buf)
-		if out != testOut {
-			t.Errorf("#%d: wrong output:\nhave %q\nwant %q\n", i, out, testOut)
+		if out != tt.out {
+			t.Errorf("#%d (%+v): wrong output:\nhave %q\nwant %q\n", i, tt, out, tt.out)
 		}
 	}
-}
-
-func TestReadImports(t *testing.T) {
-	testRead(t, readImportsTests, func(r io.Reader) ([]byte, error) { return readImports(r, true, nil) })
-}
-
-func TestReadComments(t *testing.T) {
-	testRead(t, readCommentsTests, readComments)
-}
-
-var readFailuresTests = []readTest{
-	{
-		`package`,
-		"syntax error",
-	},
-	{
-		"package p\n\x00\nimport `math`\n",
-		"unexpected NUL in input",
-	},
-	{
-		`package p; import`,
-		"syntax error",
-	},
-	{
-		`package p; import "`,
-		"syntax error",
-	},
-	{
-		"package p; import ` \n\n",
-		"syntax error",
-	},
-	{
-		`package p; import "x`,
-		"syntax error",
-	},
-	{
-		`package p; import _`,
-		"syntax error",
-	},
-	{
-		`package p; import _ "`,
-		"syntax error",
-	},
-	{
-		`package p; import _ "x`,
-		"syntax error",
-	},
-	{
-		`package p; import .`,
-		"syntax error",
-	},
-	{
-		`package p; import . "`,
-		"syntax error",
-	},
-	{
-		`package p; import . "x`,
-		"syntax error",
-	},
-	{
-		`package p; import (`,
-		"syntax error",
-	},
-	{
-		`package p; import ("`,
-		"syntax error",
-	},
-	{
-		`package p; import ("x`,
-		"syntax error",
-	},
-	{
-		`package p; import ("x"`,
-		"syntax error",
-	},
-}
-
-func TestReadFailures(t *testing.T) {
-	// Errors should be reported (true arg to readImports).
-	testRead(t, readFailuresTests, func(r io.Reader) ([]byte, error) { return readImports(r, true, nil) })
-}
-
-func TestReadFailuresIgnored(t *testing.T) {
-	// Syntax errors should not be reported (false arg to readImports).
-	// Instead, entire file should be the output and no error.
-	// Convert tests not to return syntax errors.
-	tests := make([]readTest, len(readFailuresTests))
-	copy(tests, readFailuresTests)
-	for i := range tests {
-		tt := &tests[i]
-		if !strings.Contains(tt.err, "NUL") {
-			tt.err = ""
-		}
-	}
-	testRead(t, tests, func(r io.Reader) ([]byte, error) { return readImports(r, false, nil) })
 }
 
 var packageNameTests = []struct {
@@ -320,14 +240,6 @@ var LongPackageHeaderBytes = []byte(LongPackageHeader)
 func BenchmarkReadPackageName_Long(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		readPackageName(LongPackageHeaderBytes)
-	}
-}
-
-func BenchmarkReadImports_Long(b *testing.B) {
-	r := bytes.NewReader(LongPackageHeaderBytes)
-	for i := 0; i < b.N; i++ {
-		readImports(r, true, nil)
-		r.Seek(0, 0)
 	}
 }
 
